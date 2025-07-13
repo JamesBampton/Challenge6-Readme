15fc1534a7ea008bb2771a8cc835ca8609432fd3
James Bampton <bamptini@hotmail.com>
2025-07-13 10:33:50 +0100

    found a simpler way to get the data. It seems to work  by  gettin ghte key value data in one go and formatting this into a file using new line

--- main.py
+++ main.py
@@ -2,67 +2,45 @@
 #import collections.abc
 from PyInquirer import prompt
 #from collections.abc import Mapping
-
+# List of dictionaries questions[0-x]
 questions = [
     {
         'type': 'input',
-        'name': 'title',
+        'name': 'Project',
         'message': 'Project Title',
     },
     {
         'type': 'input',
-        'name': 'desc',
+        'name': 'Description',
         'message': 'Brief Description of the project',
     },
     {
         'type': 'input',
-        'name': 'install',
+        'name': 'Install',
         'message': 'Provide installation instructions',
     },
     {
         'type': 'input',
-        'name': 'usage',
+        'name': 'Usage',
         'message': 'How to use',
     },
     {
         'type': 'list',
-        'name': 'license',
+        'name': 'License',
         'message': 'Select a license',
         'choices': ['Yes', 'No', 'Maybe'],
     },
     {
         'type': 'input',
-        'name': 'contact',
+        'name': 'Contact',
         'message': 'Add contact information',
     },
 ]
 
-# Could make this a module
-answers = prompt(questions)
-#qLength = len(answers)
-#print(answers)
-#print(qLength)
-# add a loop here to go through the answers 
-#print(f"Project title is , {answers['title']}!")
-#print(f"Project description is , {answers['desc']}!")
-    
+answers = prompt(questions) # Place the answers from the quesion prompts into 
+print (answers) #answers = {'Project': 'A', 'Description': 'B', 'Install': 'C', 'Usage': 'D', 'License': 'Yes', 'Contact': 'E'}
         
-for value in answers.values():
-    
-    
-    
-    
-    
-    
-    #text = value
-    with open("readme-jb.txt", "a") as f:
-        #f.write(f"##{answers['title']}\n")
-        f.write((str('##')))
-        f.write(f"##{answers['title']}\n")
-        f.write((str(value)))
-        #f.write('\n')
-        #f.write(text)
-        
-        #for key in answers.keys():
-        #    f.write(f"# {key}\n")
+with open("readme-jb.txt", "a") as f:
+    for key, value in answers.items(): #
+         f.write(f"## {key}\n{value}\n")
         
